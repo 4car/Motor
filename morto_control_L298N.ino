bb0e3df93d5d65df52c24262a40ef9fb34ea3e28
LinYouJ <D0952386@o365.fcu.edu.tw>
2023-10-20 19:14:40 +0800

    motor

---
+++ morto_control_L298N.ino
@@ -0,0 +1,71 @@
+ // Nano connect L298N
+ const int IN1=8;
+ const int IN2=7;
+ const int ENA=9;
+ const int IN3=5;
+ const int IN4=4;
+ const int ENB=3;
+// NX connect Nano
+int val=0;
+const int NXinput=6;
+
+// control speed
+const int PWM_FORWARD = 70;
+const int PWM_BACK=90;
+
+void setup() {
+pinMode (ENA, OUTPUT) ; 
+pinMode (IN1, OUTPUT);
+pinMode (IN2, OUTPUT); 
+
+}
+void Forward1(){  //OUTPUT1 & OUTPUT2
+analogWrite(ENA, PWM_FORWARD);
+digitalWrite(IN1,HIGH);
+digitalWrite(IN2,LOW);
+ }
+
+void Forward2(){  //OUTPUT3 & OUTPUT4
+analogWrite(ENB, PWM_FORWARD);
+digitalWrite(IN3,LOW);
+digitalWrite(IN4,HIGH);
+ }
+
+ 
+void back1(){
+analogWrite(ENA, PWM_BACK);
+digitalWrite(IN1,LOW);
+digitalWrite(IN2,HIGH);
+ }
+
+void back2(){
+analogWrite(ENB, PWM_BACK);
+digitalWrite(IN3,HIGH);
+digitalWrite(IN4,LOW);
+ }
+
+ 
+void stop1(){
+analogWrite(ENA, 0);
+digitalWrite(IN1,LOW);
+digitalWrite(IN2,LOW);
+ }
+
+void stop2(){
+analogWrite(ENB, 0);
+digitalWrite(IN3,LOW);
+digitalWrite(IN4,LOW);
+ }
+
+void loop(){
+  // nx 輸出需接上拉電阻10k~20k
+  val = digitalRead(NXinput);
+  if(val == HIGH){
+    Forward1();
+    Forward2();
+  }
+  else{
+    stop1();
+    stop2();
+  }
+}
